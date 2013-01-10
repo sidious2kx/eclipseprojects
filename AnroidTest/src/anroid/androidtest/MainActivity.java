890e3a63e47e30c3c87d9f148c4f1c18b6667e41
Dan Brooks <danger.dan_@hotmail.co.uk>
2013-01-10 18:09:36 +0000

    Cleaned up project & added code for button that triggers toast dialog

--- AnroidTest/src/anroid/androidtest/MainActivity.java
+++ AnroidTest/src/anroid/androidtest/MainActivity.java
@@ -1,18 +1,41 @@
 package anroid.androidtest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
+	private OnClickListener clickEvent = new OnClickListener(){
+		public void onClick(View v) {
+    		Context context = getApplicationContext();
+    		CharSequence text;
+
+    		switch(v.getId()) {
+	    		case R.id.button1: text = "Button pushed!"; break;
+	    		default: text="Something was pushed!";
+    		}
+
+    		int duration = Toast.LENGTH_SHORT;
+    		Toast toast = Toast.makeText(context, text, duration);
+    		toast.show();
+    	}
+		
+	};
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(clickEvent);
     }
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
